857ba4c8a08e7504002ea7411c8f5c70adb34b50
santaimany <santaimany@gmail.com>
2026-04-21 20:50:10 +0700

    feat: implement PoliService with findAll and findOne methods

--- apps/rsud-service/src/poli/poli.service.ts
+++ apps/rsud-service/src/poli/poli.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Poli } from './entities/poli.entity';
+import { Repository } from 'typeorm/repository/Repository.js';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class PoliService {}
+export class PoliService {
+    constructor(
+        @InjectRepository(Poli) private poliRepository: Repository<Poli>,
+    ) {}
+
+    findAll() {
+        return this.poliRepository.find({ where: { is_active: true } });
+    }
+
+    async findOne(id: number) {
+        const poli = await this.poliRepository.findOne({where: {id, is_active: true}});
+        if(!poli) throw new NotFoundException('Poli tidak ditemukan')
+        
+        return poli
+    }
+
+
+}
