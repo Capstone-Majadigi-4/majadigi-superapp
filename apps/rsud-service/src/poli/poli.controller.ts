9d706d36eeee245da8b2bc8f85f41a1e3065dfb3
santaimany <santaimany@gmail.com>
2026-04-21 21:55:38 +0700

    feat: implement PoliController with findAll and findOne methods

--- apps/rsud-service/src/poli/poli.controller.ts
+++ apps/rsud-service/src/poli/poli.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { PoliService } from './poli.service';
+import { success } from '../common/helpers/response.helper';
 
-@Controller('poli')
+@Controller('rsud/poli')
 export class PoliController {
   constructor(private readonly poliService: PoliService) {}
+
+  @Get()
+  async findAll() {
+    const data = await this.poliService.findAll();
+    return success(data, 'Daftar poli berhasil diambil');
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    const data = await this.poliService.findOne(id);
+    return success(data);
+  }
 }
--- apps/rsud-service/src/poli/poli.module.ts
+++ apps/rsud-service/src/poli/poli.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Poli } from './entities/poli.entity';
 import { PoliService } from './poli.service';
 import { PoliController } from './poli.controller';
 
 @Module({
-  controllers: [PoliController],
+  imports: [TypeOrmModule.forFeature([Poli])],
   providers: [PoliService],
+  controllers: [PoliController],
+  exports: [PoliService],
 })
 export class PoliModule {}
