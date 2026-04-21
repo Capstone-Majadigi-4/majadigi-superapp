f6cc7dbcaab3202fcb7d75b8dcf06e5e5966306b
santaimany <santaimany@gmail.com>
2026-04-21 20:22:12 +0700

    refactor: remove unused filters, helpers, and interfaces from shared package

--- packages/shared/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./interfaces/api-response.interface";
-export * from "./interfaces/jwt-payload.interface";
-export * from "./helpers/response.helper";
-export * from "./filters/http-exception.filter";
--- packages/shared/src/interfaces/jwt-payload.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface JwtPayload {
-  sub: string;
-  nik: string;
-  name: string;
-  iat?: number;
-  exp?: number;
-}
