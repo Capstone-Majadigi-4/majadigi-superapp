ecf49647396a902635dbab1027b709966229d30b
santaimany <santaimany@gmail.com>
2026-04-23 10:54:49 +0700

    feat: implement Kamar module with caching support for room availability

--- apps/rsud-service/src/app.module.ts
+++ apps/rsud-service/src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 import { PoliModule } from './poli/poli.module';
 import { AntreanModule } from './antrean/antrean.module';
 import { NotificationModule } from './notification/notification.module';
 import { SeederModule } from './database/seeder.module';
+import { KamarModule } from './kamar/kamar.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        stores: [createKeyv(config.get<string>('REDIS_URL', 'redis://localhost:6379'))],
+      }),
+    }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
@@ -28,6 +38,7 @@ import { SeederModule } from './database/seeder.module';
     NotificationModule,
     PoliModule,
     AntreanModule,
+    KamarModule,
   ],
 })
 export class AppModule {}
