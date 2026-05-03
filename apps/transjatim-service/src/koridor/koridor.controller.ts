668f267c02e33eb7d067c2c4cb7b022c69c4b3a2
santaimany <santaimany@gmail.com>
2026-05-03 23:57:45 +0700

    feat: add findAll and findOne methods to KoridorController for retrieving data

--- apps/transjatim-service/src/koridor/koridor.controller.ts
+++ apps/transjatim-service/src/koridor/koridor.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { KoridorService } from './koridor.service';
 
 @Controller('koridor')
 export class KoridorController {
   constructor(private readonly koridorService: KoridorService) {}
+
+  @Get()
+  findAll() {
+    return this.koridorService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.koridorService.findOne(id);
+  }
 }
