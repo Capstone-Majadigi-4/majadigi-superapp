d0b8db0fb51aef657f35d6cb61b986b935046422
santaimany <santaimany@gmail.com>
2026-05-03 23:59:06 +0700

    feat: update HalteModule to include TypeOrmModule for Halte entity

--- apps/transjatim-service/src/halte/halte.module.ts
+++ apps/transjatim-service/src/halte/halte.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Halte } from './entities/halte.entity';
 import { HalteService } from './halte.service';
 import { HalteController } from './halte.controller';
 
 @Module({
-  controllers: [HalteController],
+  imports: [TypeOrmModule.forFeature([Halte])],
   providers: [HalteService],
+  controllers: [HalteController],
+  exports: [HalteService],
 })
 export class HalteModule {}
