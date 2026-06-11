dfee6cd1b5657f21c2f15aef5f30a63ae88b6756
santaimany <santaimany@gmail.com>
2026-06-11 21:21:39 +0700

    feat: add metrics module with middleware and controller for HTTP request monitoring

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -6,9 +6,14 @@ async function bootstrap() {
     bodyParser: false,
   });
 
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
+ app.enableCors({
+   origin: [
+     'http://localhost:5173',
+     'https://majadigi-admin-dashboard.vercel.app',
+   ],
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+   credentials: true,
+ });
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
