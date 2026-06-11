bbc43d30e493bb4012777bd217305920e4825f25
santaimany <santaimany@gmail.com>
2026-06-11 22:50:43 +0700

    feat: implement findAll method in UsersService and add corresponding endpoint in UsersController

--- apps/auth-service/src/users/users.controller.ts
+++ apps/auth-service/src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll() {
+    const data = await this.usersService.findAll();
+    return {
+      status: 'success',
+      message: 'Data user berhasil diambil',
+      data: data.map((u) => ({
+        ...u,
+        nik: this.usersService.maskNik(u.nik),
+      })),
+    };
+  }
 }
